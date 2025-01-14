795693eb91e26178743f3b9d43677f0fc46a6a16
Rosa Myers <larosaardente@gmail.com>
2025-01-13 17:01:01 -0800

    Created login and register pages and associated controllers and models.

---
+++ OnTolkien/OnTolkien/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OnTolkien.Models;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "Please enter a username.")]
+    [StringLength(255)]
+    public string Username { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Please enter a password.")]
+    [DataType(DataType.Password)]
+    [Compare("ConfirmPassword")]
+    public string Password { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Please confirm your password.")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
