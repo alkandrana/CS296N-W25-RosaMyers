fa2ef1eeafa306bfe9478665ee4a4828605a7e72
Rosa Myers <larosaardente@gmail.com>
2025-01-13 17:37:24 -0800

    Added a registration page with the associated model and action methods.

---
+++ OnTolkien/OnTolkien/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using OnTolkien.Models;
+namespace OnTolkien.Controllers;
+
+public class AccountController : Controller
+{
+    
+    private UserManager<AppUser> _userManager;
+    private SignInManager<AppUser> _signInManager;
+
+    public AccountController(UserManager<AppUser> userMngr, SignInManager<AppUser> signInMngr)
+    {
+        _userManager = userMngr;
+        _signInManager = signInMngr;
+    }
+    // GET
+    public IActionResult Register()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterVM model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new AppUser { UserName = model.Username };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+        }
+        return View(model);
+    }
+}
